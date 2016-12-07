89790cdc55b583084de80a8d5e29b4659ed2e6eb
Wataru Takase <wataru.takase@kek.jp>
2016-12-07 09:27:09 +0900

    Support dashbord redirection with specified index

--- public/app.js
+++ public/app.js
@@ -21,7 +21,8 @@ uiRoutes
           currentKibanaIndex: resp.data.currentKibanaIndex,
           kibanaIndexPrefix: resp.data.kibanaIndexPrefix,
           username: resp.data.username,
-          groups: resp.data.groups
+          groups: resp.data.groups,
+          dashboard: null
         };
       });
     }
@@ -35,7 +36,23 @@ uiRoutes
           currentKibanaIndex: resp.data.currentKibanaIndex,
           kibanaIndexPrefix: resp.data.kibanaIndexPrefix,
           username: resp.data.username,
-          groups: resp.data.groups
+          groups: resp.data.groups,
+          dashboard: null
+        };
+      });
+    }
+  }
+}).when('/:suffix/:dashboard', {
+  template,
+  resolve: {
+    userInfo($route, $http) {
+      return $http.get('../api/multi_kibana_index/selection/' + $route.current.params.suffix).then(function (resp) {
+        return {
+          currentKibanaIndex: resp.data.currentKibanaIndex,
+          kibanaIndexPrefix: resp.data.kibanaIndexPrefix,
+          username: resp.data.username,
+          groups: resp.data.groups,
+          dashboard: $route.current.params.dashboard
         };
       });
     }
@@ -44,7 +61,7 @@ uiRoutes
 
 uiModules
 .get('app/multi_kibana_index', [])
-.controller('multiKibanaIndex', function ($scope, $route) {
+.controller('multiKibanaIndex', function ($scope, $route, $location) {
   $scope.title = 'Select kibana.index';
   $scope.description = 'You can select kibana.index for personal or group use. Following candidates are listed based on your username and LDAP roles.';
   let userInfo = $route.current.locals.userInfo;
@@ -52,4 +69,9 @@ uiModules
   $scope.kibanaIndexPrefix = userInfo.kibanaIndexPrefix;
   $scope.username = userInfo.username;
   $scope.groups = userInfo.groups;
+  if (userInfo.dashboard) {
+    $location.url('/');
+    $route.reload();
+    window.location.replace('/app/kibana#/dashboard/' + userInfo.dashboard);
+  }
 });
